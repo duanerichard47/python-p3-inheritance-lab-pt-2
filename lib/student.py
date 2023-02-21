edb1a67aef600e43493ce6ad3587d227a75649e1
Duane <duanerichard47@hotmail.com>
2023-02-21 14:50:39 -0500

    complete better

--- lib/student.py
+++ lib/student.py
@@ -10,22 +10,10 @@ class ChattyStudent(Student):
         super().hello()
         print("How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died...")
     def raise_hand(self):
-        super().raise_hand()
-        super().raise_hand()
-        super().raise_hand()
-        super().raise_hand()
-        super().raise_hand()
-        super().raise_hand()
-        super().raise_hand()
-        super().raise_hand()
-        super().raise_hand()
-        super().raise_hand()
+        for i in range(10):
+            super().raise_hand()
 
 
-
-        # x = range(10)
-        # n = super().raise_hand()
-        # for n in x:
-        #    return n 
+        
             
 
